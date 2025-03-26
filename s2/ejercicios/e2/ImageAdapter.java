840ea20c587852adf030624999661e49749069e2
Docjar99 <jaardeltacoch@gmail.com>
2025-03-25 23:24:36 -0500

    Create ImageAdapter.java

---
+++ s2/ejercicios/e2/ImageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.pizza;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context context;
+    private int[] images = {R.drawable.pizza1, R.drawable.pizza2, R.drawable.pizza3};
+    private String[] names = {"Pizza Pepperoni", "Pizza Cheese", "Pizza Romana"};
+
+    public ImageAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return names[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.grid_item, parent, false);
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.imageView);
+        TextView textView = convertView.findViewById(R.id.textView);
+
+        imageView.setImageResource(images[position]);
+        textView.setText(names[position]);
+
+        return convertView;
+    }
+}
